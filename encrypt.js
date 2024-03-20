53a6849426016d5f3e5102d45fb2273d87e3ab3c
densellp <densell321@gmail.com>
2024-03-20 12:49:49 -0700

    secert migration

---
+++ encrypt.js
@@ -0,0 +1,21 @@
+const sodium = require('libsodium-wrappers');
+
+const envs = process.env.ENVS;
+const secret = 'BRUH';
+const key = envs[0].key;
+
+//Check if libsodium is ready and then proceed.
+sodium.ready.then(() => {
+  // Convert the secret and key to a Uint8Array.
+  let binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL);
+  let binsec = sodium.from_string(secret);
+
+  // Encrypt the secret using libsodium
+  let encBytes = sodium.crypto_box_seal(binsec, binkey);
+
+  // Convert the encrypted Uint8Array to Base64
+  let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL);
+
+  // Print the output
+  console.log(output);
+});
