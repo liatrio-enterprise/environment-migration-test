0be572a89afd60fe3a8f35e68aa8fe3d3d4fc8ad
densellp <densell321@gmail.com>
2024-03-28 17:01:35 -0700

    script updated for reviewer issues

--- migrate.js
+++ migrate.js
@@ -2,9 +2,20 @@ const { Octokit }  = require('@octokit/rest')
 const fs = require('fs')
 const csv = require('csv-parser')
 const sodium = require('libsodium-wrappers')
+const { get } = require('http')
+const { exec } = require('child_process')
+const { TIMEOUT } = require('dns')
 
 const sourceRepo = { owner: process.env.SOURCE_ORG, repo: process.env.SOURCE_REPO }
 const targetRepo = { owner: process.env.TARGET_ORG, repo: process.env.TARGET_REPO }
+
+// console.log("Source Repo: ", sourceRepo)
+// console.log("Target Repo: ", targetRepo)
+// console.log("GH_PAT_SOURCE: ", process.env.GH_PAT_SOURCE)
+// console.log("GH_PAT_TARGET: ", process.env.GH_PAT_TARGET)
+
+// Set to true if you want to append the repo ID to the environment name
+const addRepoID = false;
 
 const octokitSource = new Octokit({
   auth: process.env.GH_PAT_SOURCE,
@@ -17,57 +28,126 @@ const octokitTarget = new Octokit({
 const secretValue = "temp"
 let newEnvs = []
 
-async function migrateEnvironments() {
-  const {data: environments} = await octokitSource.repos.getAllEnvironments(sourceRepo)
-  console.log("Environments: ", environments)
+console.log("Migrating environments from source to target repo...");
 
-  // let usersMap = new Map()
-  // try {
-  //   usersMap = await generateUserMap()
-  //   console.log("generate user map: ")
-  //   console.log(usersMap)
-  // } catch(error) {
-  //   console.error(error)
-  // }
+async function migrateEnvironments() {
+  const {data: environments} = await octokitSource.repos.getAllEnvironments(sourceRepo);
+  //console.log("Environments: ", environments);
+
+  let tempEnv = environments.environments;
+
+  const repoId = await octokitTarget.rest.repos.get({
+    owner: targetRepo.owner,
+    repo: targetRepo.repo,
+  });
+
+  tempEnv = tempEnv.map(environment => {
+    return {
+      ...environment,
+      repoID: repoId.data.id // replace 'yourRepoID' with the actual repoID
+    };
+  });
+
+  //console.log("environments NEW: ", tempEnv);  
+
+  let usersMap = new Map()
+  try {
+    usersMap = await generateUserMap()
+    //console.log("user map: ")
+    //console.log(usersMap)
+  } catch(error) {
+    console.error(error)
+  }
 
   let reviewerList = []
   let envList = []
 
-  for (const env of environments.environments) {
+  for (const env of tempEnv) {
+    //console.log("Environment: ", env)
     let wait_timer
     let envObj = { 
         env: env.name,
         reviewerList: reviewerList,
         prevent_self_review: false,
     };
+    //let emuReviewersArray = []
     if (env.protection_rules) {
-      console.log(env.protection_rules);
-      env.protection_rules.forEach((rule) => {
+      // console.log("Protection rules: ")
+      // console.log(env.protection_rules);
+      for (const rule of env.protection_rules) {
         if (rule.type === 'wait_timer') {
           wait_timer = rule.wait_timer
         } else if (rule.type === 'required_reviewers'){
           //console.log(rule.reviewers);
+          reviewerList = [];
           if (rule.prevent_self_review === true) {
             envObj.prevent_self_review = true;
           }
           for (const reviewer of rule.reviewers) {
-            //console.log(reviewer.reviewer.login);
-            reviewerList.push(reviewer.reviewer.login );
+            //console.log("reviewer:", reviewer.reviewer);
+            //console.log("reviewer login:", reviewer.reviewer.login);
+            reviewerList.push(reviewer.reviewer.login);
+            // const userOBJSource = await gatherUserData(reviewer.reviewer.login, process.env.GH_PAT_SOURCE);
+            // console.log("User email: ", userOBJSource.email)
+            // if(usersMap.has(userOBJSource.email)) {
+            //   console.log("Found a match for: ", reviewer.reviewer.login);
+              //const userEmu = usersMap.get(userOBJ.email);
+              // console.log(usersMap);
+              // const userOBJTarget = await gatherUserData(usersMap.get(userOBJSource.email), process.env.GH_PAT_TARGET);
+              // console.log("User Object Target: ", userOBJTarget);
+              // console.log(userOBJTarget);
+              // const userEmuId = userOBJTarget.id;
+              // const userEmuType =  userOBJTarget.type;
+              // console.log("Emu: ", userEmu)
+              // console.log("User ID: ", userEmuId);
+              // if (userOBJTarget.id > 0){
+              //   emuReviewersArray.push({ id: userEmuId, type: userEmuType });
+              // } else {
+              //   console.log("User not found in target ORG: ", reviewer.reviewer.login);
+              //   console.log("Adding to list on non-verified reviewers");
+              //   reviewerList.push(reviewer.reviewer.login);
+              // }
+            // } else {
+            //   console.log("No match found for: ", reviewer.reviewer.login);
+            //   console.log("Adding to list on non-verified reviewers");
+            //   reviewerList.push(reviewer.reviewer.login);
+            // }
           }
           envObj.reviewerList = reviewerList;
-          envList.push(envObj)
+          envList.push(envObj);
         }
-      });
+      }
     }
+    //console.log("reviewerList: ", reviewerList)
     env.wait_timer = wait_timer
 
     const protected_branches = env.deployment_branch_policy ? env.deployment_branch_policy.protected_branches : null
     const custom_branch_policies = env.deployment_branch_policy ? env.deployment_branch_policy.custom_branch_policies : null
 
-    await octokitSource.rest.repos.createOrUpdateEnvironment({
+    let envName = env.name;
+
+    if (addRepoID === true) {
+      envName = env.name + "-" + repoId.data.id;
+      //console.log("Env Name: ", envName);
+    }
+
+    // Uncomment When Needed (Code used for custom reviewer mapping)
+    // await octokitTarget.rest.repos.createOrUpdateEnvironment({
+    //   owner: targetRepo.owner,
+    //   repo: targetRepo.repo,
+    //   environment_name: envName,
+    //   deployment_branch_policy: env.deployment_branch_policy ? {
+    //     protected_branches: protected_branches,
+    //     custom_branch_policies: custom_branch_policies,
+    //   } : null,
+    //   wait_timer: env.wait_timer,
+    //   ...(emuReviewersArray.length > 0 ? { reviewers: emuReviewersArray } : {})
+    // });
+
+    await octokitTarget.rest.repos.createOrUpdateEnvironment({
       owner: targetRepo.owner,
       repo: targetRepo.repo,
-      environment_name: env.name,
+      environment_name: envName,
       deployment_branch_policy: env.deployment_branch_policy ? {
         protected_branches: protected_branches,
         custom_branch_policies: custom_branch_policies,
@@ -76,42 +156,46 @@ async function migrateEnvironments() {
     });
   }
 
-  
-  const secrets = await getEnvironmentSecrets(environments)
+  // process.exit(0);
+
+  const secrets = await getEnvironmentSecrets(tempEnv);
   const secretsEncrypt = await processEnvs(secrets, secretValue)
   await migrateSecrets(secretsEncrypt)
   
-  const variables = await getEnvironmentVariables(environments)
+  const variables = await getEnvironmentVariables(tempEnv);
   await migrateVariables(variables)
   
   await generateIssuesForRequiredReviewers(envList)
   await generateIssuesForEnvironmentSecrets(secrets)
+  console.log("Migration complete!")
 }
 
-// async function generateUserMap() {
-//   return new Promise((resolve, reject) => {
-//       const usersMap = new Map();
-//       fs.createReadStream('users.csv')
-//       .pipe(csv())
-//       .on('data', (row) => {
-//           usersMap.set(row['mannequin-user'], row['target-user']);
-//       })
-//       .on('end', () => {
-//           //console.log("generated usersMap");
-//           resolve(usersMap);
-//       })
-//       .on('error', (error) => {
-//           reject(error);
-//       });
-//   });
-// }
+async function generateUserMap() {
+  let usersMap = new Map()
+  return new Promise((resolve, reject) => {
+    fs.createReadStream('users.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      usersMap.set(row.saml_name_id, row.emu)
+    })
+    .on('end', () => {
+      resolve(usersMap)
+    })
+  })
+}
 
 async function getEnvironmentSecrets(environments) {
   let envs = []
+  let envName = '';
 
-  for (const env of environments.environments ) {
+  for (const env of environments ) {
+    envName = env.name;
+    if (addRepoID === true) {
+      envName = env.name + "-" + env.repoID;
+      //console.log("Env Name: ", envName);
+    }
     let envObj = {
-      name: env.name,
+      name: envName,
       secrets: [],
       key_id: '',
       key: '',
@@ -202,10 +286,16 @@ async function migrateSecrets(secrets) {
 
 async function getEnvironmentVariables(environments) {
   let envs = []
+  let envName = '';
 
-  for (const env of environments.environments) {
+  for (const env of environments) {
+    envName = env.name;
+    if (addRepoID === true) {
+      envName = env.name + "-" + env.repoID;
+      //console.log("Env Name: ", envName);
+    }
     let envObj = {
-      name: env.name,
+      name: envName,
       vars: [],
     }
 
@@ -276,15 +366,43 @@ async function generateIssuesForRequiredReviewers(envList) {
     }
 
     if (reviewers) {
+      console.log("EnvName: ", env.env);
       const issueResult = await octokitTarget.rest.issues.create({
           owner: targetRepo.owner,
           repo: targetRepo.repo,
           title: 'Update reviewers for environment: ' + `\`${env.env}\``,
           body: issueBody,
       });
-      console.log(issueResult.data);
+      //console.log(issueResult.data);
     }
   }
+}
+
+async function gatherUserData(login, token) {
+  return new Promise((resolve, reject) => {
+    const username = login;
+    const command = `curl -H "Authorization: token ${token}" https://api.github.com/users/${username}`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+        return;
+      }
+
+      const userData = JSON.parse(stdout);
+
+      let userOBJ = {
+        login: userData.login || '',
+        id: userData.id || 0,
+        type: userData.type || '',
+        email: userData.email || '',
+      };
+
+      //console.log(`User Object in gatherUSerData Function: `, userOBJ);
+      resolve(userOBJ);
+    });
+  });
 }
 
 migrateEnvironments().catch(console.error);
